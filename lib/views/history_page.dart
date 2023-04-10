c003953bf3ca6c74fdcc6ced0cd726c876391914
unknown <sivaharicr7@gmail.com>
2023-04-10 22:55:03 +0530

    history added

--- lib/views/history_page.dart
+++ lib/views/history_page.dart
@@ -1,69 +1,40 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
-class BookingHistoryPage extends StatefulWidget {
+class BookingHistoryPage extends StatelessWidget {
   final String qrData;
 
   BookingHistoryPage({required this.qrData});
-
-  @override
-  _BookingHistoryPageState createState() => _BookingHistoryPageState();
-}
-
-class _BookingHistoryPageState extends State<BookingHistoryPage> {
-  List<Map<String, dynamic>> bookings = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadBookings();
-  }
-
-  void _loadBookings() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? bookingsJson = prefs.getString('bookings');
-    if (bookingsJson != null) {
-      List<dynamic> bookingsList = jsonDecode(bookingsJson);
-      setState(() {
-        bookings = bookingsList.cast<Map<String, dynamic>>();
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Booking History'),
+        backgroundColor: Colors.green,
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            child: QrImage(
-              data: widget.qrData
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Booking Details:',
+              style: TextStyle(fontSize: 20),
             ),
-          ),
-          Expanded(
-            child: bookings.isNotEmpty
-                ? ListView.builder(
-              itemCount: bookings.length,
-              itemBuilder: (BuildContext context, int index) {
-                Map<String, dynamic> booking = bookings[index];
-                return ListTile(
-                  title: Text(booking['spotName']),
-                  subtitle: Text(booking['date']),
-                  trailing: Text('\$${booking['price']}'),
-                );
+            SizedBox(height: 16),
+            Text(
+              qrData,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              child: Text('Back'),
+              onPressed: () {
+                Navigator.pop(context);
               },
-            )
-                : Center(
-              child: Text('No bookings found.'),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
